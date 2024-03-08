e00d3c1cbd741dae17e6185bf1d137f5155d094e
Maxime Vorwerk <Maxime Vorwerk>
2024-03-08 17:57:42 +0100

    now with actual solution

--- change_making.jl
+++ change_making.jl
@@ -19,6 +19,27 @@ function ordered_count_change(coinset::Vector{Int}, target::Int)
     return buffer_get(buffer, buffer_offset, 1)
 end
 
+function count_change(coinset::Vector{Int}, target::Int)
+    n_coins = length(coinset)
+    buffer = zeros(Int, n_coins, target)
+    for i ∈ 1:target
+        buffer[1, i] = 1
+    end
+    for i ∈ 2:n_coins
+        coin = coinset[i]
+        for j ∈ 1:target
+            if j < coin
+                buffer[i, j] = buffer[i-1, j]
+            elseif j == coin
+                buffer[i, j] = buffer[i-1, j] + 1
+            else
+                buffer[i, j] = buffer[i-1, j] + buffer[i, j - coin]
+            end
+        end
+    end
+    return buffer[n_coins, target]
+end
+
 function buffer_append(buffer::Vector{Int}, offset::Int, value::Int)
     offset = mod(offset-1, length(buffer))
     buffer[offset+1] = value
